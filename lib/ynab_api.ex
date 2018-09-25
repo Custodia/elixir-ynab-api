c8d62eb200ed3bbb3b91c34e2e2d6c3d6c346a34
Jani Mäkelä <janimakelaa@gmail.com>
2018-09-25 18:25:01 +0300

    Initialize application
    We want the API wrapper to be stateful so we can make full use of delta
    requests.

---
+++ lib/ynab_api.ex
@@ -0,0 +1,18 @@
+defmodule YnabApi do
+  @moduledoc """
+  Documentation for YnabApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> YnabApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ynab_api_test.exs
@@ -0,0 +1,8 @@
+defmodule YnabApiTest do
+  use ExUnit.Case
+  doctest YnabApi
+
+  test "greets the world" do
+    assert YnabApi.hello() == :world
+  end
+end
