e989cb144ea7aedee82530a9a2fe03ab60359d2d
Jani Mäkelä <janimakelaa@gmail.com>
2018-11-11 13:56:54 +0200

    Add ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule YnabApi.MixProject do
     [
       {:httpoison, "~> 1.0"},
       {:jason, "~> 1.1"},
-      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
